406b7ee050d535a9af126c45723b7f8aa791ecc7
tom <aarakkanolla@gmail.com>
2018-08-02 19:54:58 +0000

    Added unique name in the code

--- src/com/vaannila/domain/User.java
+++ src/com/vaannila/domain/User.java
@@ -13,7 +13,7 @@ public class User {
 	public String getName() {
 		return name;
 	}
-        public String getpersonsName() {
+        public String getuniqueName() {
                 return name;
         }
 	public void setName(String name) {
