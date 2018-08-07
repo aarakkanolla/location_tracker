4ca272950bc03ee2b91f8ffc6854d0e94a6c603c
tom <aarakkanolla@gmail.com>
2018-08-07 20:29:14 +0000

    abhi name

--- src/com/vaannila/domain/User.java
+++ src/com/vaannila/domain/User.java
@@ -13,7 +13,7 @@ public class User {
 	public String getName() {
 		return name;
 	}
-        public String getnickName() {
+        public String getabhiName() {
                 return name;
         }
 	public void setName(String name) {
