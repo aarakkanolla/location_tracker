76274ed51f6fca55684978aa459e7b5207777f54
tom <aarakkanolla@gmail.com>
2018-08-07 19:36:15 +0000

    reverted back to nick

--- src/com/vaannila/domain/User.java
+++ src/com/vaannila/domain/User.java
@@ -13,7 +13,7 @@ public class User {
 	public String getName() {
 		return name;
 	}
-        public String getpetName() {
+        public String getnickName() {
                 return name;
         }
 	public void setName(String name) {
